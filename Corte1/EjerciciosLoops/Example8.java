16eee20d5d0faf200c070eba6a3dece2e8da9611
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 16:43:40 -0500

    Create Example8.java

---
+++ Corte1/EjerciciosLoops/Example8.java
@@ -0,0 +1,11 @@
+// Java program demonstrating break 
+class Example8 { 
+  public static void main(String[] args) {       
+    for (int i = 1; i <= 10; ++i) { 
+      if (i == 5) { 
+        break; 
+      }       
+      System.out.println(i); 
+    }    
+  } 
+} 
