005bfc6ec626519b6f68e6422ce229f534856e90
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 16:42:33 -0500

    Create Example5.java

---
+++ Corte1/EjerciciosLoops/Example5.java
@@ -0,0 +1,9 @@
+// Program to print a text 5 times 
+class Example5 { 
+  public static void main(String[] args) { 
+    int n = 5; 
+    for (int i = 1; i <= n; ++i) { 
+      System.out.println("Java is fun"); 
+    } 
+  } 
+} 
