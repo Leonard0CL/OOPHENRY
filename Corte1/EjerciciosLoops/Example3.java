098c2080e2fc2bdd32268066b33a229350e8015e
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 16:41:41 -0500

    Create Example3.java

---
+++ Corte1/EjerciciosLoops/Example3.java
@@ -0,0 +1,10 @@
+// Java Program to display numbers from 1 to 5 
+class Example3 { 
+  public static void main(String[] args) { 
+    int i = 1, n = 5; 
+    do { 
+      System.out.println(i); 
+      i++; 
+    } while(i <= n); 
+  } 
+} 
