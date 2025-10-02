ee9f8b695098d5c6d997755f0bc217bccd8dcffd
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 17:21:08 -0500

    Create OperacionesAritmeticas.java

---
+++ Corte1/Calculadora/OperacionesAritmeticas.java
@@ -0,0 +1,26 @@
+class OperacionesBasicas:
+    def _init_(self):
+        self.val=0
+        self.val2=0
+        self.r=0
+
+    def getVal(self):
+        return self.val
+    
+    def setVal(self, valor):
+        self.val=valor
+
+    def getVal2(self):
+        return self.val2
+    
+    def setVal2(self, valor):
+        self.val2=valor 
+
+    def getR(self):
+        return self.r
+    
+    def setR(self, valor):
+        self.r= valor
+
+    def sumar(self, a, b):
+        self.r= a + b
