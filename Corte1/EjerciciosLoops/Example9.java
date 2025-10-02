8f43ef5ad4f5727f3ef8a61edc222e5eff4d227c
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 16:44:09 -0500

    Create Example9.java

---
+++ Corte1/EjerciciosLoops/Example9.java
@@ -0,0 +1,11 @@
+// Java program demonstrating continue 
+class Example9 { 
+  public static void main(String[] args) { 
+    for (int i = 1; i <= 10; ++i) { 
+      if (i > 4 && i < 9) { 
+        continue; 
+      } 
+      System.out.println(i); 
+    } 
+  } 
+} 
