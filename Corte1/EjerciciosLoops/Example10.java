dda95d1e11cde2707a2abefa5f7dbc8d573bf04f
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 16:44:30 -0500

    Create Example10.java

---
+++ Corte1/EjerciciosLoops/Example10.java
@@ -0,0 +1,18 @@
+// Java program to compute the sum of 5 positive numbers 
+import java.util.Scanner; 
+
+class Example10 { 
+  public static void main(String[] args) { 
+    Double number, sum = 0.0; 
+    Scanner input = new Scanner(System.in); 
+    for (int i = 1; i < 6; ++i) { 
+      System.out.print("Enter number " + i + " : "); 
+      number = input.nextDouble(); 
+      if (number <= 0.0) { 
+        continue; 
+      } 
+      sum += number; 
+    } 
+    System.out.println("Sum = " + sum); 
+  } 
+} 
