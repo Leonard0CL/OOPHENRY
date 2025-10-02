b5992270661d7702fd02a86d863ccae477ec326a
Leonard0CL <erickleon1026@gmail.com>
2025-10-02 16:43:21 -0500

    Create Example7.java

---
+++ Corte1/EjerciciosLoops/Example7.java
@@ -0,0 +1,11 @@
+// Program to find the sum of natural numbers from 1 to 1000. 
+class Example7 { 
+  public static void main(String[] args) { 
+    int sum = 0; 
+    int n = 1000; 
+    for (int i = 1; i <= n; ++i) { 
+      sum += i;     
+    }    
+    System.out.println("Sum = " + sum); 
+  } 
+} 
